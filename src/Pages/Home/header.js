f7542f7ff65dbf638ad787ab2938e0d8da37d032
withsme <tmdal9953@gmail.com>
2023-04-17 15:24:36 +0900

    feat : 헤더

--- src/Pages/Home/header.js
+++ src/Pages/Home/header.js
@@ -1,24 +1,49 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+import { call, signout } from '../../service/ApiService';
 
-function Header() {
-  const [login, setLogin] = useState(false);
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { item: [], IsLogin: false};
+  }
 
-  return (
-    <Container>
-      <Title>
-        <Link to="/" style={{ color: "#6c8074", textDecoration: "none" }}>Tasty</Link>
-      </Title>
-      <Ul>
-        {login ? (
-          <li>Log out</li>  
+  componentDidMount() {
+    call("/auth/user", "GET", null).then((response) =>
+      this.setState({item:response.data})
+    );
+  }
+
+  render() {
+    return (
+      <Container>
+        <Title>
+          <Link to="/" style={{ color: "#6c8074", textDecoration: "none" }}>Tasty</Link>
+        </Title>
+        <Ul>
+          {this.state.item.length > 0 ? (
+            <>
+              <li>
+                <Link to="/modify" style={{ color: "#6c8074", textDecoration: "none", paddingRight: "35px" }}>
+                  My page
+                </Link>
+              </li>
+              <li>
+                <Button color='inherit' onClick={signout}>
+                  Log out
+                </Button>
+              </li>
+            </>
           ) : (
-          <li><Link to="/login" style={{ color: "#6c8074", textDecoration: "none" }}>Log in</Link></li>
-        )}
-      </Ul>
-    </Container>
-  );
+            <li>
+              <Link to="/login" style={{ color: "#6c8074", textDecoration: "none" }}>Log in</Link>
+            </li>
+          )}
+        </Ul>
+      </Container>
+    );
+  }
 }
 
 const Container = styled.div`
@@ -43,10 +68,20 @@ const Ul = styled.ul`
   margin: 0px;
   padding: 0px;
   display: flex;
+  color: #6c8074;
   font-size: 22px;
   font-weight: 100;
   padding: 32px 50px 24px 24px;
   list-style: none;
+`
+
+const Button = styled.button`
+  font-family: 'Titan One', 'cursive';
+  font-weight: 100;
+  font-size: 22px;
+  color: #6c8074;
+  border: transparent;
+  background-color: transparent;
 `
 
 export default Header;
