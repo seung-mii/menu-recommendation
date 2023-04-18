7bf75a3ee0e7cb3ed59fec9dbea3f2bcee4528a9
withsme <tmdal9953@gmail.com>
2023-04-18 16:20:21 +0900

    feat : 회원정보 수정 및 회원탈퇴 기능

--- Backend/src/main/java/com/example/brand/controller/MemberController.java
+++ Backend/src/main/java/com/example/brand/controller/MemberController.java
@@ -1,10 +1,18 @@
 package com.example.brand.controller;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -77,6 +85,54 @@ public class MemberController {
 					.error("Login failed")
 					.build();
 			return ResponseEntity.badRequest().body(responseDTO);
+		}
+	}
+
+	@GetMapping("/user")
+	public ResponseEntity<?> retrieve(@AuthenticationPrincipal String id) {
+
+		List<Member> entities = MemberService.retrieve(id);
+		List<MemberDTO> dtos = entities.stream().map(MemberDTO::new).collect(Collectors.toList());
+
+		ResponseDTO<MemberDTO> response = ResponseDTO.<MemberDTO>builder().data(dtos).build();
+
+		// HTTP Status 200 ���·� response �� �����Ѵ�.
+		return ResponseEntity.ok().body(response);
+	}
+
+	@PutMapping
+	public ResponseEntity<?> update(@AuthenticationPrincipal String id, @RequestBody MemberDTO MemberDTO) {
+		try {
+			Member entity = MemberDTO.toEntity(MemberDTO);
+			entity.setM_key(id);
+
+			List<Member> entities = MemberService.update(entity);
+			List<MemberDTO> dtos = entities.stream().map(MemberDTO::new).collect(Collectors.toList());
+
+			ResponseDTO<MemberDTO> response = ResponseDTO.<MemberDTO>builder().data(dtos).build();
+
+			return ResponseEntity.ok().body(response);
+		} catch (Exception e) {
+			ResponseDTO<MemberDTO> response = ResponseDTO.<MemberDTO>builder().error(e.getMessage()).build();
+			return ResponseEntity.badRequest().body(response);
+		}
+	}
+
+	@DeleteMapping
+	public ResponseEntity<?> delete(@AuthenticationPrincipal String id, @RequestBody MemberDTO MemberDTO) {
+		try {
+			Member entity = MemberDTO.toEntity(MemberDTO);
+			entity.setM_key(id);
+
+			List<Member> entities = MemberService.delete(entity);
+			List<MemberDTO> dtos = entities.stream().map(MemberDTO::new).collect(Collectors.toList());
+
+			ResponseDTO<MemberDTO> response = ResponseDTO.<MemberDTO>builder().data(dtos).build();
+
+			return ResponseEntity.ok().body(response);
+		} catch (Exception e) {
+			ResponseDTO<MemberDTO> response = ResponseDTO.<MemberDTO>builder().error(e.getMessage()).build();
+			return ResponseEntity.badRequest().body(response);
 		}
 	}
 }
--- Backend/src/main/java/com/example/brand/service/MemberService.java
+++ Backend/src/main/java/com/example/brand/service/MemberService.java
@@ -1,9 +1,13 @@
 package com.example.brand.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.example.brand.dto.MemberDTO;
+import com.example.brand.model.FRestaurantEntity;
 import com.example.brand.model.Member;
 import com.example.brand.persistence.MemberRepository;
 
@@ -14,27 +18,65 @@ import lombok.extern.slf4j.Slf4j;
 public class MemberService {
 	@Autowired
 	private MemberRepository MemberRepository;
-
+	
 	public Member create(final Member Member) {
-		if (Member == null || Member.getUserID() == null) {
+		if(Member == null || Member.getUserID() == null) {
 			throw new RuntimeException("Invalid arguments");
 		}
-		final String userID = Member.getUserID();
-		if (MemberRepository.existsByUserID(userID)) {
-			log.warn("userID already exists {}", userID);
-			throw new RuntimeException("userID already exists");
+		final String email = Member.getUserID();
+		if(MemberRepository.existsByUserID(email)) {
+			log.warn("Email already exists {}",email);
+			throw new RuntimeException("Email already exists");
 		}
-
+		
 		return MemberRepository.save(Member);
 	}
-
+	
 	public Member getByCredentials(final String userID, final String password, final PasswordEncoder encoder) {
 		final Member originalUser = MemberRepository.findByUserID(userID);
-
-		if (originalUser != null && encoder.matches(password, originalUser.getPassword())) {
+		
+		if(originalUser != null && encoder.matches(password, originalUser.getPassword())) {
 			return originalUser;
 		}
-
+		
 		return null;
+	}
+
+	public List<Member> retrieve(final String id) {
+		return MemberRepository.findByID(id);
+	}
+	
+	public List<Member> update(final Member entity) {
+		validate(entity);
+		if (MemberRepository.existsById(entity.getM_key())) {
+			MemberRepository.save(entity);
+		}
+		else
+			throw new RuntimeException("Unknown id");
+		
+		//return repository.findById(entity.getM_key());
+		return MemberRepository.findByID(entity.getM_key());		
+	}
+	
+	public List<Member> delete(final Member entity) {
+		validate(entity);
+		if (MemberRepository.existsById(entity.getM_key())) {
+			MemberRepository.deleteById(entity.getM_key());
+		}
+		else
+			throw new RuntimeException("Unknown id");
+		
+		return MemberRepository.findByID(entity.getM_key());		
+	}
+
+	public void validate(final Member entity) {
+		if(entity == null ) {
+			log.warn("Entity cannot be null.");
+			throw new RuntimeException("Entity cannot be null.");
+		}
+		if(entity.getUserID() == null) {
+			log.warn("Unknown user.");
+			throw new RuntimeException("Unknown user.");
+		}
 	}
 }
