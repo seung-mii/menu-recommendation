c73fa575131d4961c6136f1f5a609a1606a79c22
withsme <tmdal9953@gmail.com>
2023-04-20 18:30:13 +0900

    feat : 리뷰 조회 및 수정 기능

--- Backend/src/main/java/com/example/brand/controller/PostController.java
+++ Backend/src/main/java/com/example/brand/controller/PostController.java
@@ -6,7 +6,11 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,29 +29,78 @@ import lombok.extern.slf4j.Slf4j;
 public class PostController {
 	@Autowired
 	private PostService service;
+	
 
 	@PostMapping
 	public ResponseEntity<?> createBrand(
 			@AuthenticationPrincipal String r_key,
 			@RequestBody PostDTO dto) {
 		try {
-			PostEntity entity = PostDTO.toEntity(dto);
-			entity.setP_key(null);
-			entity.setR_key(r_key);
+		PostEntity entity = PostDTO.toEntity(dto);
+		entity.setP_key(null);
+		entity.setR_key(r_key);
+		
+		List<PostEntity> entities = service.create(entity);
+		
+		List<PostDTO> dtos = entities.stream().map(PostDTO::new).collect(Collectors.toList());
+		log.info("Log:entities => dtos ok!");		
 
-			List<PostEntity> entities = service.create(entity);
-
-			List<PostDTO> dtos = entities.stream().map(PostDTO::new).collect(Collectors.toList());
-			log.info("Log:entities => dtos ok!");
-
-			ResponseDTO<PostDTO> response = ResponseDTO.<PostDTO>builder().data(dtos).build();
-			log.info("Log:responsedto ok!");
-
-			return ResponseEntity.ok().body(response);
+		ResponseDTO<PostDTO> response = ResponseDTO.<PostDTO>builder().data(dtos).build();
+		log.info("Log:responsedto ok!");
+		
+		return ResponseEntity.ok().body(response);
 		} catch (Exception e) {
 			String error = e.getMessage();
 			ResponseDTO<PostDTO> response = ResponseDTO.<PostDTO>builder().error(error).build();
 			return ResponseEntity.badRequest().body(response);
 		}
+	}
+	
+	@GetMapping
+	public ResponseEntity<?> retrieveBrand(@AuthenticationPrincipal String r_key) {
+		
+		List<PostEntity> entities = service.retrieve(r_key);
+		List<PostDTO> dtos = entities.stream().map(PostDTO::new).collect(Collectors.toList());
+
+		ResponseDTO<PostDTO> response = ResponseDTO.<PostDTO>builder().data(dtos).build();
+		
+		// HTTP Status 200 ���·� response �� �����Ѵ�.
+		return ResponseEntity.ok().body(response);
+	}
+	
+	
+	@GetMapping("/all")
+	public ResponseEntity<?> retrieveBrandAll(@AuthenticationPrincipal String r_key) {
+		
+		List<PostEntity> entities = service.retrieveAll(r_key);
+		List<PostDTO> dtos = entities.stream().map(PostDTO::new).collect(Collectors.toList());
+
+		ResponseDTO<PostDTO> response = ResponseDTO.<PostDTO>builder().data(dtos).build();
+		
+		// HTTP Status 200 ���·� response �� �����Ѵ�.
+		return ResponseEntity.ok().body(response);
+	}
+	
+	@PutMapping
+	public ResponseEntity<?> updateBrand(
+			@AuthenticationPrincipal String r_key,
+			@RequestBody PostDTO dto) {
+		try {
+		PostEntity entity = PostDTO.toEntity(dto);
+		entity.setR_key(r_key);
+		
+		List<PostEntity> entities = service.update(entity);
+		
+		List<PostDTO> dtos = entities.stream().map(PostDTO::new).collect(Collectors.toList());
+
+		ResponseDTO<PostDTO> response = ResponseDTO.<PostDTO>builder().data(dtos).build();
+		
+		return ResponseEntity.ok().body(response);
+		} catch (Exception e) {
+			String error = e.getMessage();
+			ResponseDTO<PostDTO> response = ResponseDTO.<PostDTO>builder().error(error).build();
+			return ResponseEntity.badRequest().body(response);
+		}
+		
 	}
 }
--- Backend/src/main/java/com/example/brand/service/PostService.java
+++ Backend/src/main/java/com/example/brand/service/PostService.java
@@ -13,24 +13,53 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Service
 public class PostService {
-
+	
 	@Autowired
 	private PostRepository repository;
-
+	
 	public List<PostEntity> create(final PostEntity entity) {
-		// Validations
+		//Validations
 		validate(entity);
 		repository.save(entity);
-		// return repository.findById(entity.getP_key());
+		//return repository.findById(entity.getP_key());
 		return repository.findByR_key(entity.getR_key());
 	}
-
+	
+	public List<PostEntity> retrieve(final String r_key) {
+		return repository.findByR_key(r_key);
+	}
+	
+	public List<PostEntity> retrieveAll(final String r_key) {
+		return repository.findByR_keyAll(r_key);
+	}
+	
+	public List<PostEntity> update(final PostEntity entity) {
+		//Validations
+		validate(entity);
+		if (repository.existsById(entity.getP_key())) {
+			repository.save(entity);
+		}
+		else
+			throw new RuntimeException("Unknown id");
+		
+		//return repository.findById(entity.getP_key());
+		return repository.findByR_key(entity.getR_key());		
+	}
+	
+	public List<PostEntity> delete(final PostEntity entity) {
+		if(repository.existsById(entity.getP_key()))
+			repository.deleteById(entity.getP_key());
+		else
+			throw new RuntimeException("id does not exist");
+		
+		return repository.findByR_key(entity.getR_key());
+	}
 	public void validate(final PostEntity entity) {
-		if (entity == null) {
+		if(entity == null ) {
 			log.warn("Entity cannot be null.");
 			throw new RuntimeException("Entity cannot be null.");
 		}
-		if (entity.getR_key() == null) {
+		if(entity.getR_key() == null) {
 			log.warn("Unknown user.");
 			throw new RuntimeException("Unknown user.");
 		}
