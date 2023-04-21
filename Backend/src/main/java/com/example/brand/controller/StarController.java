8a046454e42f6a4624a1be3b5688ac977fda59fb
withsme <tmdal9953@gmail.com>
2023-04-21 12:30:13 +0900

    feat : 별점 조회 및 수정 기능

--- Backend/src/main/java/com/example/brand/controller/StarController.java
+++ Backend/src/main/java/com/example/brand/controller/StarController.java
@@ -6,7 +6,11 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,29 +28,101 @@ import lombok.extern.slf4j.Slf4j;
 public class StarController {
 	@Autowired
 	private StarService service;
-
+	
 	@PostMapping
 	public ResponseEntity<?> createBrand(
 			@AuthenticationPrincipal String r_key,
 			@RequestBody StarDTO dto) {
 		try {
-			StarEntity entity = StarDTO.toEntity(dto);
-			entity.setS_key(null);
-			entity.setR_key(r_key);
+		StarEntity entity = StarDTO.toEntity(dto);
+		entity.setS_key(null);
+		entity.setR_key(r_key);
+		
+		List<StarEntity> entities = service.create(entity);
+		
+		List<StarDTO> dtos = entities.stream().map(StarDTO::new).collect(Collectors.toList());
+		log.info("Log:entities => dtos ok!");		
 
-			List<StarEntity> entities = service.create(entity);
-
-			List<StarDTO> dtos = entities.stream().map(StarDTO::new).collect(Collectors.toList());
-			log.info("Log:entities => dtos ok!");
-
-			ResponseDTO<StarDTO> response = ResponseDTO.<StarDTO>builder().data(dtos).build();
-			log.info("Log:responsedto ok!");
-
-			return ResponseEntity.ok().body(response);
+		ResponseDTO<StarDTO> response = ResponseDTO.<StarDTO>builder().data(dtos).build();
+		log.info("Log:responsedto ok!");
+		
+		return ResponseEntity.ok().body(response);
 		} catch (Exception e) {
 			String error = e.getMessage();
 			ResponseDTO<StarDTO> response = ResponseDTO.<StarDTO>builder().error(error).build();
 			return ResponseEntity.badRequest().body(response);
 		}
+	}
+	
+	@GetMapping
+	public ResponseEntity<?> retrieveBrand(@AuthenticationPrincipal String r_key) {
+		
+		List<StarEntity> entities = service.retrieve(r_key);
+		List<StarDTO> dtos = entities.stream().map(StarDTO::new).collect(Collectors.toList());
+
+		ResponseDTO<StarDTO> response = ResponseDTO.<StarDTO>builder().data(dtos).build();
+		
+		// HTTP Status 200 ���·� response �� �����Ѵ�.
+		return ResponseEntity.ok().body(response);
+		
+	}
+	
+	@GetMapping("/one")
+	public ResponseEntity<?> retrieveBrandOne(@AuthenticationPrincipal String r_key) {
+		
+		List<StarEntity> entities = service.retrieveOne(r_key);
+		List<StarDTO> dtos = entities.stream().map(StarDTO::new).collect(Collectors.toList());
+
+		ResponseDTO<StarDTO> response = ResponseDTO.<StarDTO>builder().data(dtos).build();
+		
+		// HTTP Status 200 ���·� response �� �����Ѵ�.
+		return ResponseEntity.ok().body(response);
+		
+	}
+
+	@PutMapping
+	public ResponseEntity<?> updateBrand(
+			@AuthenticationPrincipal String r_key,
+			@RequestBody StarDTO dto) {
+		try {
+		StarEntity entity = StarDTO.toEntity(dto);
+		entity.setR_key(r_key);
+		
+		List<StarEntity> entities = service.update(entity);
+		
+		List<StarDTO> dtos = entities.stream().map(StarDTO::new).collect(Collectors.toList());
+
+		ResponseDTO<StarDTO> response = ResponseDTO.<StarDTO>builder().data(dtos).build();
+		
+		return ResponseEntity.ok().body(response);
+		} catch (Exception e) {
+			String error = e.getMessage();
+			ResponseDTO<StarDTO> response = ResponseDTO.<StarDTO>builder().error(error).build();
+			return ResponseEntity.badRequest().body(response);
+		}
+		
+	}
+	@DeleteMapping
+	public ResponseEntity<?> deleteBrand(
+			@AuthenticationPrincipal String r_key,
+			@RequestBody StarDTO dto) {
+		try {
+			StarEntity entity = StarDTO.toEntity(dto);
+
+			entity.setR_key(r_key);
+
+			List<StarEntity> entities = service.delete(entity);
+			
+			List<StarDTO> dtos = entities.stream().map(StarDTO::new).collect(Collectors.toList());
+		
+			ResponseDTO<StarDTO> response = ResponseDTO.<StarDTO>builder().data(dtos).build();
+			
+			return ResponseEntity.ok().body(response);
+			} catch (Exception e) {
+				String error = e.getMessage();
+				ResponseDTO<StarDTO> response = ResponseDTO.<StarDTO>builder().error(error).build();
+				return ResponseEntity.badRequest().body(response);
+			}			
+	
 	}
 }
--- Backend/src/main/java/com/example/brand/service/StarService.java
+++ Backend/src/main/java/com/example/brand/service/StarService.java
@@ -13,24 +13,53 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Service
 public class StarService {
-
+	
 	@Autowired
 	private StarRepository repository;
-
+	
 	public List<StarEntity> create(final StarEntity entity) {
-		// Validations
+		//Validations
 		validate(entity);
 		repository.save(entity);
-		// return repository.findById(entity.getId());
+		//return repository.findById(entity.getId());
 		return repository.findByR_key(entity.getR_key());
 	}
+	
+	public List<StarEntity> retrieve(final String star) {
+		return repository.findByR_key(star);
+	}
+	
+	public List<StarEntity> retrieveOne(final String star) {
+		return repository.findByR_keyOne(star);
+	}
+	
+	public List<StarEntity> update(final StarEntity entity) {
+		//Validations
+		validate(entity);
+		if (repository.existsById(entity.getS_key())) {
+			repository.save(entity);
+		}
+		else
+			throw new RuntimeException("Unknown id");
+		
+		//return repository.findById(entity.getId());
+		return repository.findByR_key(entity.getR_key());		
+	}
+	
+	public List<StarEntity> delete(final StarEntity entity) {
+		if(repository.existsById(entity.getS_key()))
+			repository.deleteById(entity.getS_key());
+		else
+			throw new RuntimeException("id does not exist");
 
+		return repository.findByR_key(entity.getR_key());
+	}
 	public void validate(final StarEntity entity) {
-		if (entity == null) {
+		if(entity == null ) {
 			log.warn("Entity cannot be null.");
 			throw new RuntimeException("Entity cannot be null.");
 		}
-		if (entity.getR_key() == null) {
+		if(entity.getR_key() == null) {
 			log.warn("Unknown user.");
 			throw new RuntimeException("Unknown user.");
 		}
